933ea912a29f9be7eabef14565eb78429a623900
BT_1N3_33 <sample@bths.edu>
2025-01-10 10:48:49 -0500

    changed print

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ajkldfjkljkl");
         System.out.println("ni hao");
-        System.out.println("hola");
+        System.out.println("iowroiioqwriux");
         System.out.println("hey there");
     }
 }
