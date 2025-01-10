14f40ba4a5f63d9d577a3ddc435504a1cea29801
BT_1N3_34 <sample@bths.edu>
2025-01-10 10:38:21 -0500

    added print

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("hello");
+        System.out.println("hola");
     }
 }
