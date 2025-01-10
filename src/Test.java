8dc32f8cde3de7fe7e79720600de2b1b46d21f1d
BT_1N3_33 <sample@bths.edu>
2025-01-10 10:47:33 -0500

    changed print

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("ajkldfjkljkl");
         System.out.println("ni hao");
         System.out.println("hola");
         System.out.println("hey there");
